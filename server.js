96168226f968009eaf6713f5baebbbcba94d5aa7
prakash929523 <banapuramprakashreddy@gmail.com>
2018-07-20 00:46:26 -0400

    middleware file added

--- server.js
+++ server.js
@@ -1,18 +1,12 @@
 var express = require('express');
 var app = express();
 var PORT = 3000;
+
+
+var middleware = require('./middleware.js');
 //object middleware
 //route level middleware
-var middleware = {
-	requireAuthentication: function(req, res, next){
-		console.log('private route hit!');
-		next();
-	},
-	logger : function(req, res, next){
-		console.log('Request: ' + new Date().toString() + ' ' +req.method+ '' + req.originalUrl);
-		next();
-	}
-};
+
 
 
 //whenever route occure private route can hit every time
